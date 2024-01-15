e288d68b6a85be0e2f710cdc87d3deec8a0521ae
DanielVierh <daniel.vierheilig89@yahoo.de>
2024-01-15 22:21:51 +0100

    Prototype and demolition right

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+const btn_setBrick = document.getElementById('btn_setBrick');
+const bricks = document.querySelectorAll('.brick');
+let oldBrick;
+
+btn_setBrick.addEventListener('click', ()=> {
+    //* im Bereich des vorherigen = old_X + width
+    //* Rechts abbruchstelle = Wenn (currentbrick.x + width) > (old_X + width) DANN - (currentbrick.x + width) - (old_X + width)
+    const current_brick = document.querySelector(".current").getBoundingClientRect();
+    const old_brick = document.querySelector(".old").getBoundingClientRect();
+
+    // Abbruch rechts
+    if((current_brick.x + current_brick.width) > (old_brick.x + old_brick.width)) {
+        console.log('Abbruch rechts');
+        const abboard_right = (current_brick.x + current_brick.width) - (old_brick.x + old_brick.width);
+        console.log('abboard_right', abboard_right);
+    }
+
+    // Abbruch links
+
+
+    console.log('current_brick', current_brick);
+    console.log('old_brick', old_brick);
+})
+
